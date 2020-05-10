2d523a16b3ab47473abee21f85f6f64bb48cfe58
Kyle McLean <kyledmclean@gmail.com>
2020-05-10 05:05:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
 
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount = '0')
+    @total = 0 
+    @discount = discount
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity = '1')
+    cost_total = (price * quantity)
+    @total += cost_total
+    
+  end
+    
+  
+end
